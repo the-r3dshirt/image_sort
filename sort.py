38d656e6f6260af7c26eb817d9ddc9c773eb163d
Sam Knowles <sam.knowles@gmail.com>
2015-05-06 23:11:10 +1200

    Refactoring: create dirs and copy files.

--- sort.py
+++ sort.py
@@ -11,14 +11,19 @@ Extras: https://pypi.python.org/pypi/ExifRead
 '''
 
 
-def print_tags(tags):
+def create_filename(file_path, date):
     '''
-    Utility function, print out tag keys and values.
+    Create unique filename using day of month and hash of file content
     '''
-    for tag in tags.keys():
-        if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
-            print ("Key: {}, value {}".format(tag, tags[tag]))
-    print ('---------------- "" ------------------')
+    with open(file_path, 'rb') as open_file:
+        file_hash = sha1(open_file.read()).hexdigest()
+
+    _, extension = os.path.splitext(file_path)
+    filename = '{}_{}{}'.format(
+        date.strftime('%d'),
+        file_hash,
+        extension.lower())
+    return filename
 
 
 def build_structure(structure, root, file):
@@ -26,10 +31,7 @@ def build_structure(structure, root, file):
     Build directory structure from file meta data, in this case the
     date the photo was taken.
     '''
-
     file_path = os.path.join(root, file)
-    #m_time = os.stat(file_path).st_mtime
-
     with open(file_path, 'rb') as open_file:
         tags = exifread.process_file(open_file)
         #print_tags(tags)
@@ -38,22 +40,36 @@ def build_structure(structure, root, file):
             created_at = str(tags['Image DateTime']) # eg. 2013:12:26 15:40:34
         except KeyError:
             print ('No date data found for: {}'.format(file_path))
+            # Default to using mtime?
+            #m_time = os.stat(file_path).st_mtime
+            return structure
 
     # Convert string date into datetime object
     date = datetime.datetime.strptime(created_at, '%Y:%m:%d %H:%M:%S')
+    new_filename = create_filename(file_path, date)
 
-    # Build folder structure from date
+    # Build structure for directories and files from date
     year = str(date.year)
     month = '{}. {}'.format(date.strftime('%m'), date.strftime('%B')) # eg. 02. February
     if year in structure:
         if month not in structure[year]:
-            structure[year].update({month: [file_path]})
+            structure[year].update({month: [(file_path, new_filename)]})
         else:
-            structure[year][month].append(file_path)
+            structure[year][month].append((file_path, new_filename))
     else:
-        structure[year] = {month: [file_path]}
+        structure[year] = {month: [(file_path, new_filename)]}
 
     return structure
+
+
+def print_tags(tags):
+    '''
+    Utility function, print out tag keys and values.
+    '''
+    for tag in tags.keys():
+        if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
+            print ("Key: {}, value {}".format(tag, tags[tag]))
+    print ('---------------- "" ------------------')
 
 
 if __name__ == "__main__":
@@ -71,41 +87,24 @@ if __name__ == "__main__":
     structure = {}
     for root, sub_dirs, files in os.walk(data_in):
         for f in files:
-
             structure = build_structure(structure, root, f)
 
-            # Get file m time
-            file_path = os.path.join(root, f)
-            m_time = os.stat(file_path).st_mtime
+    print(structure)
 
-            # Build folder structure from file m time
-            date = datetime.datetime.fromtimestamp(m_time)
-            year = str(date.year)
-
-            folder_path = '{}{}/{}. {}'.format(
+    # Create directories
+    for year in structure.keys():
+        for month in structure[year].keys():
+            directory = '{}{}/{}/'.format(
                 data_out,
                 year,
-                date.strftime('%m'),
-                date.strftime('%B'))
-
-            # Create directories
+                month)
             try:
-                os.makedirs(folder_path)
+                os.makedirs(directory)
             except FileExistsError:
                 pass
 
-            # Create filename
-            with open(file_path, 'rb') as open_file:
-                file_hash = sha1(open_file.read()).hexdigest()
-
-            _, extension = os.path.splitext(file_path)
-            filename = '/{}_{}{}'.format(
-                date.strftime('%d'),
-                file_hash,
-                extension.lower())
-
-            # Copy files to new homes
-            #print('copying: ' + folder_path + '/'+ filename)
-            copy2(file_path, folder_path + '/'+ filename)
-
-    print(structure)
+            # Copy files into new directories
+            data = structure[year][month]
+            for original, filename in data:
+                print('{}: {}{}'.format(original, directory, filename))
+                copy2(original, directory + filename)
